22ce541c2cbdc035959c1ac8ea360766ab606d29
Pradeep_Kumar <pradeepvijay170@gmail.com>
2025-06-17 12:12:53 +0530

    Update Blogs.js

--- frontend/src/Pages/Blogs.js
+++ frontend/src/Pages/Blogs.js
@@ -54,7 +54,7 @@ function Blogs() {
             <div className="col-lg-4" key={blog._id}>
               <div className="service_single_item style_two style_three">
                 <div className="service_thumb">
-                  <img src={`http://localhost:5000/${blog.imageUrl}`} alt={blog.title} style={{width:'100%'}} />
+                  <img src={`https://consulting-4rbe.onrender.com/${blog.imageUrl}`} alt={blog.title} style={{width:'100%'}} />
                 </div>
                 <div className="service_content">
                   <h3>{new Date(blog.date).toLocaleDateString()}</h3>
