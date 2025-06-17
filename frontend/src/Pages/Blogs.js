6fcc0bef1d41c291b4b92912370b07b7c4ac854e
Pradeep_Kumar <pradeepvijay170@gmail.com>
2025-06-17 12:02:23 +0530

    Update Blogs.js

--- frontend/src/Pages/Blogs.js
+++ frontend/src/Pages/Blogs.js
@@ -12,7 +12,7 @@ function Blogs() {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/blogs');
+        const response = await fetch('https://consulting-4rbe.onrender.com/api/blogs');
         if (response.ok) {
           const data = await response.json();
           setBlogs(Array.isArray(data) ? data : []);
